bd82ff9b8c5cb587fc4a8eced613285db12f4888
bigwhoman <hm.keshvari@gmail.com>
2022-06-19 09:25:26 +0430

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 # to start the file from python
 def beeep():
     frequency = 2500  # Set Frequency To 2500 Hertz
-    duration = 5000  # Set Duration To 1000 ms == 1 second
+    duration = 3000  # Set Duration To 1000 ms == 1 second
     winsound.Beep(frequency, duration)
 
 
@@ -24,23 +24,21 @@ def site_scrape():
     options.headless = True
     options.add_argument("--window-size=1920,1200")
     driver = webdriver.Firefox()
-    driver.get("")
-    all_links = driver.find_element(by=By.CLASS_NAME, value='promo-desc')
-    last = all_links.text
+    driver.get("https://www.tiwall.com/showcase?filters=s:music")
     while True:
         try:
-            time.sleep(10)
-            all_links = driver.find_element(by=By.CLASS_NAME, value='promo-desc')
-            print(all_links.text)
-            if last != driver.find_element(by=By.CLASS_NAME, value='promo-desc').text:
-                os.system("start task_done.mp3")
-                exit_prog(driver)
-                return
-            driver.refresh()
-        except:
-            os.system("start error_voice.mp3")
+            time.sleep(5)
+            driver.find_element(by=By.CSS_SELECTOR, value='[data-saleurn="/s/homayoun.shajarian"]')
+            os.system("start task_done.mp3")
             exit_prog(driver)
             return
+        except Exception as e:
+            if not str(e).find("unable to locate"):
+                print(e)
+                exit_prog(driver)
+                return
+            print("there has been an exception :: ",str(e))
+            driver.refresh()
 
 
 site_scrape()
